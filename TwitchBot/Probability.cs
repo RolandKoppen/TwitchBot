0b268937861906ab2c73bb3f0eef7e3a9cd36cd8
Roland Koppen <r.koppen@allsolutions.nl>
2016-09-26 21:20:40 +0200

    Implement DropRate or Chance that something 'Might Occur', return true or false for the action to happen.

--- TwitchBot/Probability.cs
+++ TwitchBot/Probability.cs
@@ -8,17 +8,32 @@ namespace TwitchBot
 {
     class Probability
     {
+        // Identify Random Number Generator
+        Random Rand;
 
         // Constructor
         public Probability()
         {
-
+            // Initialize Random Number Generator
+            Rand = new Random();
         }
 
         // Destructor
         ~Probability()
         {
 
+        }
+
+        public bool ProbabilityPercentage(int Percent)
+        {
+            if (Rand.Next(1,101) <= Percent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
